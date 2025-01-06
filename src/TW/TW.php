3a3c7bd12477c1aaeb2df506d38be1ccc2003494
Felipe Martins <felipedesmartins@gmail.com>
2025-01-05 22:18:14 -0300

    feat: create tailwind class

---
+++ src/TW/TW.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace PHTML\TW;
+
+use PHTML\TAG;
+
+/*
+ * Tailwind CSS
+ */
+class TW extends TAG
+{
+  private array $fixedClasses;
+  private array $classesAvailable;
+  private array $classesOnDarkMode;
+
+  public function __toString(): string
+  {
+    return $this->render();
+  }
+
+  public function render(): string
+  {
+    $this->loadFixedClasses();
+    return parent::render();
+  }
+
+  private function loadFixedClasses(): void
+  {
+    if (!isset($this->fixedClasses)) {
+      return;
+    }
+
+    foreach ($this->fixedClasses as $class) {
+      $this->addClass($class);
+    }
+  }
+
+  public function setFixedClasses(array $classes): self
+  {
+    if (!isset($this->fixedClasses)) {
+      $this->fixedClasses = $classes;
+    }
+
+    return $this;
+  }
+}
